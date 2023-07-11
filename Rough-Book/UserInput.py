565e0b75ef7e1e794e471108b2bd77bd7e662ebb
pramod <pramodacharya808@gmail.com>
2023-07-11 19:25:19 +0530

    Added User INput

---
+++ Rough-Book/UserInput.py
@@ -0,0 +1,10 @@
+print("Enter 2 numbers")
+a = int(input())
+b = int(input())
+print(a + b)
+
+print("Enter your first name and last name")
+first = input()
+last = input()
+
+print(first, last, sep="-")
