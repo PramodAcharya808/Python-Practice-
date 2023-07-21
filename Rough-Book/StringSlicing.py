93e545d81bb610eafeb220757c18bbc76f66ef01
pramod <pramodacharya808@gmail.com>
2023-07-21 21:04:38 +0530

    Python update

---
+++ Rough-Book/StringSlicing.py
@@ -0,0 +1,15 @@
+"""
+String Slicing
+-> Normal slicing using START & END-1 range of the string index
+-> Negative slicing using START & END-1 range of the string index
+"""
+
+names = "Pramod, Acharya"
+# Normal slicing
+print(names[0:6])
+
+# Without specifying the START range of the string
+print(names[:6])
+
+# Negative slicing
+print(names[-7:-1])
