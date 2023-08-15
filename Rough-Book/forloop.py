9acd3fefa7537e0177cd4f46d79b8b77f2b069eb
pramod <pramodacharya808@gmail.com>
2023-08-15 19:36:02 +0530

    python practice

--- Rough-Book/forloop.py
+++ Rough-Book/forloop.py
@@ -1,5 +1,5 @@
 # multiplication table of N
-num = int(input('Enter the number to print its multiplication table'))
+num = int(input('Enter the number to print its multiplication table: '))
 
 for i in range(1, 11):
     product = num * i
