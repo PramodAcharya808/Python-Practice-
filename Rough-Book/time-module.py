7eaa038c35b30cafff3e1dc6181a6fcc1b3f39f3
pramod <pramodacharya808@gmail.com>
2023-08-15 18:32:31 +0530

    python practice

--- Rough-Book/time-module.py
+++ Rough-Book/time-module.py
@@ -1,7 +1,13 @@
 import time
+
 current = time.strftime('%H : %M : %S')
-print('Current time is: '+current)
+print('Current time is: ' + current)
 hr = int(time.strftime('%H'))
 if 0 <= hr < 12:
     print('Good Morning !')
-elif 12 <= hr < 18:
+elif 12 <= hr < 16:
+    print('Good After noon !')
+elif 4 <= hr < 19:
+    print('Good Evening !')
+else:
+    print('Good Night !!')
