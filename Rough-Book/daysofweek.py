1667f1de9a76f76e7d6d9cafdd37c443cd49a402
pramod <pramodacharya808@gmail.com>
2023-08-15 18:41:44 +0530

    python practice

--- Rough-Book/daysofweek.py
+++ Rough-Book/daysofweek.py
@@ -1,4 +1,4 @@
-day = int(input('Enter the day number in a week'))
+day = int(input('Enter the day number in a week: '))
 match day:
     case 1: print('MONDAY')
     case 2: print('TUESDAY')
@@ -6,5 +6,7 @@ match day:
     case 4: print('THURSDAY')
     case 5: print('FRIDAY')
     case 6: print('SATURDAY')
-    case 7: print('SUNDAY')
+    case 7:
+        print('SUNDAY')
+        print('Hurrayyy ...Its WEEKEND')
     case _: print('Please enter a valid day !!')
