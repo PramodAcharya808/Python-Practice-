b85fc295278df9eb46e0076abc260406b263707b
pramod <pramodacharya808@gmail.com>
2023-08-15 19:46:00 +0530

    python practice

--- Rough-Book/while.py
+++ Rough-Book/while.py
@@ -1,20 +1,28 @@
-day = int(input('Enter the day number in a week: '))
-while day > 3:
+day = int(1)
+while 1 <= day <= 7:
+    day = int(input('Enter the day number in a week: '))
     match day:
         case 1:
             print('MONDAY')
+            exit()
         case 2:
             print('TUESDAY')
+            exit()
         case 3:
             print('WEDNESDAY')
+            exit()
         case 4:
             print('THURSDAY')
+            exit()
         case 5:
             print('FRIDAY')
+            exit()
         case 6:
             print('SATURDAY')
+            exit()
         case 7:
             print('SUNDAY')
             print('Hurray ...Its WEEKEND')
+            exit()
         case _:
             print('Please enter a valid day !!')
